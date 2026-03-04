e4b1c6bae00dc46c92623a78cc4bd4b183addf17
Neo Gonzales <gonzalesni@students.nu-clark.edu.ph>
2026-03-04 21:35:44 +0800

    Appointment 2.0

--- StockSense/StockSense/Models/Appointment.cs
+++
@@ -1,8 +0,0 @@
-﻿
-public class Appointment
-{
-    public int Id { get; set; }
-    public string CustomerName { get; set; } = string.Empty;
-    public DateTime AppointmentDate { get; set; } = DateTime.Now;
-    public string ServiceType { get; set; } = "General Checkup";
-}
